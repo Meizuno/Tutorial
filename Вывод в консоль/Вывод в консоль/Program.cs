5ff66581a30cfa6eab7be6e409fcf5d6463b8e8d
Meizuno <michess@ukr.net>
2019-01-13 19:05:49 +0200

    Создал программу вывода на консоль

---
+++ Вывод в консоль/Вывод в консоль/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Вывод_в_консоль
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
